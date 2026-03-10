5860ca9c1ec69a1576cd67fe7eb8ac3fcaf9b0b6
Amjad Twalo <amjad.t@gmail.com>
2026-03-10 19:35:12 +0900

    move shields to boards/shields for discovery

--- flake.nix
+++ flake.nix
@@ -11,62 +11,67 @@
   outputs =
     { nixpkgs, zmk-nix, ... }:
     let
-      # Use same system as reference
-      system = "x86_64-apple-darwin"; # Set to user's mac system
-      pkgs = nixpkgs.legacyPackages.${system};
-
-      # Boards and Shields configuration
-      keyboards = {
-        photon = {
-          board = "photon//zmk";
-        };
-        photon_peripheral = {
-          board = "photon//zmk";
-          extraCmakeFlags = [ "-DEXTRA_CONF_FILE=${./config/boards/cannonkeys/photon/photon_peripheral.conf}" ];
-        };
-        photon_dongle = {
-          board = "xiao_ble//zmk";
-          shield = "photon_dongle rgbled_adapter";
-        };
-        dongle_reset = {
-          board = "xiao_ble//zmk";
-          shield = "settings_reset";
-        };
-      };
-
-      mkKeyboardFirmware =
-        name:
-        {
-          board,
-          shield ? null,
-          extraCmakeFlags ? [ ],
-          ...
-        }:
-        let
-          builders = zmk-nix.legacyPackages.${system};
-        in
-        builders.buildKeyboard {
-          inherit
-            name
-            board
-            shield
-            extraCmakeFlags
-            ;
-          src = pkgs.lib.cleanSource ./.;
-          config = "config";
-          # This hash might need update later if west.yml changes
-          zephyrDepsHash = "sha256-otsUf6aEeOmAUOV76YNXaYANAIhDWX+mm4ye8GoJ7WM=";
-        };
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
     in
     {
-      packages.${system} =
+      packages = forAllSystems (system:
         let
+          pkgs = nixpkgs.legacyPackages.${system};
+          builders = zmk-nix.legacyPackages.${system};
+
+          # Boards and Shields configuration
+          keyboards = {
+            photon = {
+              board = "photon//zmk";
+            };
+            photon_peripheral = {
+              board = "photon//zmk";
+              extraCmakeFlags = [ "-DEXTRA_CONF_FILE=${./config/boards/cannonkeys/photon/photon_peripheral.conf}" ];
+            };
+            photon_dongle = {
+              board = "xiao_ble//zmk";
+              shield = "photon_dongle rgbled_adapter";
+            };
+            dongle_reset = {
+              board = "xiao_ble//zmk";
+              shield = "settings_reset";
+            };
+          };
+
+          mkKeyboardFirmware =
+            name:
+            {
+              board,
+              shield ? null,
+              extraCmakeFlags ? [ ],
+              ...
+            }:
+            builders.buildKeyboard {
+              inherit
+                name
+                board
+                shield
+                extraCmakeFlags
+                ;
+              src = pkgs.lib.cleanSource ./.;
+              config = "config";
+              # This hash depends on the contents of config/west.yml
+              zephyrDepsHash = "sha256-0+UOYhtuhQWexrIqj2PsKSHtjQug9I4JnXi6kuKXIVo=";
+            };
+
           firmwarePackages = pkgs.lib.mapAttrs mkKeyboardFirmware keyboards;
         in
         firmwarePackages // {
           default = firmwarePackages.photon_dongle;
-        };
+        });
 
-      devShells.${system}.default = zmk-nix.devShells.${system}.default;
+      devShells = forAllSystems (system: {
+        default = zmk-nix.devShells.${system}.default;
+      });
     };
 }
